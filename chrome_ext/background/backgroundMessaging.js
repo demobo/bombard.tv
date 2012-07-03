2c68673be3b8ecc51f8e71fd18a2768eefb16858
Jiahao Li <lijiahao90@gmail.com>
2012-07-02 22:23:53 -0700

    demobo code commented out

--- chrome_ext/background/backgroundMessaging.js
+++ chrome_ext/background/backgroundMessaging.js
@@ -1,21 +1,21 @@
-chrome.extension.onConnect.addListener(function(port) {
-  if (port.name === 'scripts') {
-	  setController();
-	  console.log('demobo background init');
-    port.onMessage.addListener(function(msg) {
-      console.log(msg.cmd,msg.string);
-      if (msg.cmd=='disconnect') {
-    	  $.demobo.disconnect();
-      } else if (msg.cmd=='connect') {
-    	  $.demobo.connect();
-    	  setController();
-      } else {
-        alert('unhandled message to background.html: ' + JSON.stringify(msg));
-      }
-    });
-  } else {
-    alert('unknown attempt to connect. port.name must equal \'scripts\' ');
-  }
-});
+//chrome.extension.onConnect.addListener(function(port) {
+//  if (port.name === 'scripts') {
+//	  setController();
+//	  console.log('demobo background init');
+//    port.onMessage.addListener(function(msg) {
+//      console.log(msg.cmd,msg.string);
+//      if (msg.cmd=='disconnect') {
+//    	  $.demobo.disconnect();
+//      } else if (msg.cmd=='connect') {
+//    	  $.demobo.connect();
+//    	  setController();
+//      } else {
+//        alert('unhandled message to background.html: ' + JSON.stringify(msg));
+//      }
+//    });
+//  } else {
+//    alert('unknown attempt to connect. port.name must equal \'scripts\' ');
+//  }
+//});
 
 
