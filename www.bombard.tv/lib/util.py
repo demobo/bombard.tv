a4dbada06ec5963e0e74cc9491dd2824418d6783
Jeff Lin <jeff@demobo.com>
2012-07-17 15:02:21 -0700

    new home page

---
+++ www.bombard.tv/lib/util.py
@@ -0,0 +1,59 @@
+import os
+import cgi
+import wsgiref.handlers
+import datetime
+from django.utils import simplejson
+from google.appengine.ext.webapp import template
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+
+def convertToHumanReadable(date_time):
+    """
+    converts a python datetime object to the 
+    format "X days, Y hours ago"
+
+    @param date_time: Python datetime object
+
+    @return:
+        fancy datetime:: string
+    """
+    current_datetime = datetime.datetime.now()
+    delta = str(current_datetime - date_time)
+    if delta.find(',') > 0:
+        days, hours = delta.split(',')
+        days = int(days.split()[0].strip())
+        hours, minutes = hours.split(':')[0:2]
+    else:
+        hours, minutes = delta.split(':')[0:2]
+        days = 0
+    days, hours, minutes = int(days), int(hours), int(minutes)
+    datelets = []
+    years, months, xdays = None, None, None
+    plural = lambda x: 's' if x != 1 else ''
+    if days >= 365:
+        years = int(days / 365)
+        datelets.append('%d year%s' % (years, plural(years)))
+        days = days % 365
+    if days >= 30 and days < 365:
+        months = int(days / 30)
+        datelets.append('%d month%s' % (months, plural(months)))        
+        days = days % 30
+    if not years and days > 0 and days < 30:
+        xdays = days
+        datelets.append('%d day%s' % (xdays, plural(xdays)))        
+    if not (months or years) and hours != 0:
+        datelets.append('%d hour%s' % (hours, plural(hours)))        
+    if not (xdays or months or years):
+        datelets.append('%d minute%s' % (minutes, plural(minutes)))        
+    return ', '.join(datelets) + ' ago.'
+
+def unique_content(array):
+    unique_results = []
+    contents = []
+    for obj in array:
+        if obj.content.strip() not in contents:
+            contents.append(obj.content.strip())
+            unique_results.append(obj)
+    return unique_results
